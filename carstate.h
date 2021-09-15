bad0b2c3278940e5bbbcc13814791b08e3130d8d
Lukas <lukas.wikander@astazero.com>
2021-09-15 18:28:19 +0200

    Update carstate.h

--- carstate.h
+++ carstate.h
@@ -45,10 +45,6 @@ public:
     const QPointF getStoppingPointForTurnRadius(const double turnRadius) const;
     inline double getMinTurnRadiusRear() const { return qMax(qMin(getAxisDistance() / tanf(getMaxSteeringAngle()), mMinTurnRadiusRear), pow(getSpeed(), 2)/(0.21*9.81)); }
 
-	// Other
-	void setOrientationKnown(const bool known = true) { mIsOrientationKnown = known; }
-	bool isOrientationKnown() const { return mIsOrientationKnown; }
-
 private:
 	double mRearOverhang = 0.0; //[m]
 	double mFrontOverhang = 0.0; //[m]
