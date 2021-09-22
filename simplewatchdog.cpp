b209ccb5bcb0be5e5a72b1a14498d4d1a970305f
Marvin Damschen <marvin.damschen@ri.se>
2021-09-22 12:03:17 +0200

    Implement SimpleWatchdog: warns when EventLoop is slowed down

---
+++ simplewatchdog.cpp
@@ -0,0 +1,38 @@
+#include "simplewatchdog.h"
+#include <QTime>
+#include <QDebug>
+
+SimpleWatchdog::SimpleWatchdog(QObject *parent) : QObject(parent)
+{
+    connect(&mWatchdogTimer, &QTimer::timeout, [this](){
+        static QTime last = QTime::currentTime();
+        int timeTaken_ms = QTime::currentTime().msecsSinceStartOfDay() - last.msecsSinceStartOfDay();
+
+        if (timeTaken_ms > timeout_ms + timeout_tolerance_ms) {
+            qDebug() << "WARNING: SimpleWatchdog timed out, EventLoop slowed down? Time taken:" << timeTaken_ms << "ms (time out:" << timeout_ms << "ms, tolerance:" << timeout_tolerance_ms << "ms).";
+            emit timeout(timeTaken_ms);
+        }
+        last = QTime::currentTime();
+    });
+    mWatchdogTimer.start(timeout_ms);
+}
+
+int SimpleWatchdog::getTimeout() const
+{
+    return timeout_ms;
+}
+
+void SimpleWatchdog::setTimeout(const int &value_ms)
+{
+    timeout_ms = value_ms;
+}
+
+int SimpleWatchdog::getTimeoutTolerance() const
+{
+    return timeout_tolerance_ms;
+}
+
+void SimpleWatchdog::setTimeoutTolerance(const int &value)
+{
+    timeout_tolerance_ms = value;
+}
---
+++ simplewatchdog.h
@@ -0,0 +1,29 @@
+#ifndef SIMPLEWATCHDOG_H
+#define SIMPLEWATCHDOG_H
+
+#include <QObject>
+#include <QTimer>
+
+class SimpleWatchdog : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SimpleWatchdog(QObject *parent = nullptr);
+
+    int getTimeout() const;
+    void setTimeout(const int &value_ms);
+
+    int getTimeoutTolerance() const;
+    void setTimeoutTolerance(const int &value_ms);
+
+signals:
+    void timeout(int timeTaken_ms);
+
+private:
+    QTimer mWatchdogTimer;
+    int timeout_ms = 20;
+    int timeout_tolerance_ms = 5;
+
+};
+
+#endif // SIMPLEWATCHDOG_H
