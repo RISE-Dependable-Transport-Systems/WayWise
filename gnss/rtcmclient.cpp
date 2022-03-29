3ee604c8dea882011a99fe3b5eac9a265271aa68
Marvin Damschen <marvin.damschen@ri.se>
2022-03-29 12:29:31 +0200

    RtcmClient: read ENU ref from RTCM stream

--- gnss/rtcmclient.cpp
+++ gnss/rtcmclient.cpp
@@ -5,7 +5,35 @@ RtcmClient::RtcmClient(QObject *parent) : QObject(parent)
 {
     connect(&mTcpSocket, &QTcpSocket::readyRead, [this]{
         QByteArray data =  mTcpSocket.readAll();
-//        qDebug() << data;
+
+        // Make sure to skip "ICY 200 OK" when connection to NTRIP
+        static bool skipFirstReply = true;
+        if (skipFirstReply) {
+            skipFirstReply = false;
+            return;
+        }
+
+        // Try to read 1005 or 1006 from stream to get base station position
+        // See RTKLIB for how RTCM is decoded (https://github.com/tomojitakasu/RTKLIB)
+        // We are not CRC checking here (getting data via TCP anyways)
+        // TODO: wait for rest of incomplete message?
+        static bool foundReferenceStationInfo = false;
+        if (!foundReferenceStationInfo) {
+            const char* dataPtr = data.constData();
+            while ((dataPtr = std::find(dataPtr, data.constEnd(), RTCM3_PREAMBLE)) != data.constEnd()) {
+                if (dataPtr + 5 < data.constEnd()) {
+                    int length = getbitu(dataPtr, 14, 10) + 1; // number of bytes inkl. crc
+                    int type = getbitu(dataPtr, 24, 12);
+                    if (dataPtr + length < data.constEnd() && (type == 1005 || type == 1006)) {
+                        llh_t baseLlh = decodeLllhFromReferenceStationInfo(data.mid(dataPtr - data.constData(), length));
+//                        qDebug() << baseLlh.latitude << baseLlh.longitude << baseLlh.height << dataPtr - data.data() << length;
+                        foundReferenceStationInfo = true;
+                        emit baseStationPosition(baseLlh);
+                    }
+                }
+                dataPtr++;
+            }
+        }
         emit rtcmData(data);
     });
 
@@ -86,4 +114,75 @@ QString RtcmClient::getCurrentHost() const
 qint16 RtcmClient::getCurrentPort() const
 {
     return mCurrentPort;
+}
+
+unsigned int RtcmClient::getbitu(const char *buff, int pos, int len) {
+    unsigned int bits=0;
+    int i;
+
+    for (i = pos;i < pos + len;i++) {
+        bits = (bits << 1) + ((buff[i / 8] >> (7 - i % 8)) & 1u);
+    }
+
+    return bits;
+}
+
+int RtcmClient::getbits(const char *buff, int pos, int len) {
+    unsigned int bits = getbitu(buff, pos, len);
+
+    if (len <= 0 || 32 <= len || !(bits & (1u << (len - 1)))) {
+        return (int)bits;
+    }
+
+    return (int)(bits | (~0u << len)); // extend sign
+}
+
+double RtcmClient::getbits_38(const char *buff, int pos) {
+    return (double)getbits(buff, pos, 32) * D(64.0) + getbitu(buff, pos + 32, 6);
+}
+
+llh_t RtcmClient::decodeLllhFromReferenceStationInfo(const QByteArray data)
+{
+    double p0 = 0.0;
+    double p1 = 0.0;
+    double p2 = 0.0;
+    int bitIdx = 24 + 12;
+    int staid;
+    int itrf;
+    llh_t llhResult = {0.0, 0.0, 0.0};
+
+    if (bitIdx + 140 <= data.size() * 8) {
+        staid = getbitu(data.constData(), bitIdx, 12); bitIdx+=12;
+        itrf  = getbitu(data.constData(), bitIdx, 6);  bitIdx+= 6+4;
+        p0    = getbits_38(data.constData(), bitIdx);  bitIdx+=38+2;
+        p1    = getbits_38(data.constData(), bitIdx);  bitIdx+=38+2;
+        p2    = getbits_38(data.constData(), bitIdx);
+
+
+        p0 *= D(0.0001);
+        p1 *= D(0.0001);
+        p2 *= D(0.0001);
+
+
+        // Convert ecef to llh
+        double e2 = FE_WGS84 * (D(2.0) - FE_WGS84);
+        double r2 = p0 * p0 + p1 * p1;
+        double z = p2;
+        double zk = 0.0;
+        double sinp = 0.0;
+        double v = RE_WGS84;
+
+        while (fabs(z - zk) >= D(1E-4)) {
+            zk = z;
+            sinp = z / sqrt(r2 + z * z);
+            v = RE_WGS84 / sqrt(D(1.0) - e2 * sinp * sinp);
+            z = p2 + v * e2 * sinp;
+        }
+
+        llhResult.latitude = (r2 > D(1E-12) ? atan(z / sqrt(r2)) : (p2 > D(0.0) ? D_PI / D(2.0) : -D_PI / D(2.0))) * D(180.0) / D_PI;
+        llhResult.longitude = (r2 > D(1E-12) ? atan2(p1, p0) : D(0.0)) * D(180.0) / D_PI;
+        llhResult.height = sqrt(r2 + z * z) - v;
+    }
+
+    return llhResult;
 }
--- gnss/rtcmclient.h
+++ gnss/rtcmclient.h
@@ -4,6 +4,15 @@
 #include <QObject>
 #include <QTcpSocket>
 #include <QHostAddress>
+#include "../coordinatetransforms.h"
+
+#ifndef D
+#define D(x) 						((double)x##L)
+#endif
+
+#ifndef D_PI
+#define D_PI						D(3.14159265358979323846)
+#endif
 
 struct NtripConnectionInfo {
     QString user;
@@ -21,6 +30,7 @@ public:
     bool connectWithInfoFromFile(QString filePath);
     bool isConnected();
     void disconnect();
+    static llh_t decodeLllhFromReferenceStationInfo(const QByteArray data);
 
     QString getCurrentHost() const;
 
@@ -28,12 +38,20 @@ public:
 
 signals:
     void rtcmData(const QByteArray &data);
+    void baseStationPosition(const llh_t &baseStationPosition);
 
 private:
     QTcpSocket mTcpSocket;
     QString mCurrentHost;
     qint16 mCurrentPort;
     NtripConnectionInfo mCurrentNtripConnectionInfo;
+
+    // For parsing RTCMv3 (from RTKLIB)
+    static const char RTCM3_PREAMBLE = 0xD3;
+    static unsigned int getbitu(const char *buff, int pos, int len);
+    static int getbits(const char *buff, int pos, int len);
+    static double getbits_38(const char *buff, int pos);
+
 };
 
 #endif // RTCMCLIENT_H
