89b0234ce7cfc9769f80cf72fe62abe6907ff4a1
rickaha <rickard.hall@ri.se>
2022-11-07 18:56:15 +0100

    MavsdkVehicleServer: handle MAVLINK_MSG_ID_COMMAND_LONG, MAV_CMD_DO_SET_MISSION_CURRENT

--- communication/mavsdkvehicleserver.h
+++ communication/mavsdkvehicleserver.h
@@ -11,16 +11,18 @@
 #include <QSharedPointer>
 #include <QTimer>
 #include <mavsdk/mavsdk.h>
-#include <mavsdk/server_component.h>
+#include <mavsdk/plugins/action_server/action_server.h>
+#include <mavsdk/plugins/mavlink_passthrough/mavlink_passthrough.h>
+#include <mavsdk/plugins/mission_raw_server/mission_raw_server.h>
 #include <mavsdk/plugins/param_server/param_server.h>
 #include <mavsdk/plugins/telemetry_server/telemetry_server.h>
-#include <mavsdk/plugins/action_server/action_server.h>
-#include <mavsdk/plugins/mission_raw_server/mission_raw_server.h>
-#include "waywise.h"
-#include "vehicles/vehiclestate.h"
+#include <mavsdk/server_component.h>
+
+#include "autopilot/multiwaypointfollower.h"
 #include "sensors/gnss/ubloxrover.h"
-#include "autopilot/waypointfollower.h"
 #include "vehicles/controller/movementcontroller.h"
+#include "vehicles/vehiclestate.h"
+#include "waywise.h"
 
 class MavsdkVehicleServer : public QObject
 {
@@ -28,9 +30,10 @@ class MavsdkVehicleServer : public QObject
 public:
     explicit MavsdkVehicleServer(QSharedPointer<VehicleState> vehicleState);
     void setUbloxRover(QSharedPointer<UbloxRover> ubloxRover);
-    void setWaypointFollower(QSharedPointer<WaypointFollower> waypointFollower);
+    void setWaypointFollower(QSharedPointer<MultiWaypointFollower> waypointFollower);
     void setMovementController(QSharedPointer<MovementController> movementController);
     void setManualControlMaxSpeed(double manualControlMaxSpeed_ms);
+    void mavResult(MAV_RESULT result);
 
 signals:
     void startWaypointFollower(bool fromBeginning); // to enable starting from MAVSDK thread
@@ -39,6 +42,7 @@ signals:
     void clearRouteOnWaypointFollower();
     void startFollowPoint();
     void resetHeartbeat();
+    void missionCurrentCommand(const float autopilotID);
 
 private:
     mavsdk::Mavsdk mMavsdk;
@@ -48,11 +52,12 @@ private:
     std::shared_ptr<mavsdk::ActionServer> mActionServer;
     std::shared_ptr<mavsdk::ParamServer> mParamServer;
     std::shared_ptr<mavsdk::MissionRawServer> mMissionRawServer;
+    std::shared_ptr<mavsdk::MavlinkPassthrough> mMavlinkPassthrough;
     QTimer mPublishMavlinkTimer;
 
     QSharedPointer<VehicleState> mVehicleState;
     QSharedPointer<UbloxRover> mUbloxRover;
-    QSharedPointer<WaypointFollower> mWaypointFollower;
+    QSharedPointer<MultiWaypointFollower> mWaypointFollower;
     QSharedPointer<MovementController> mMovementController;
 
     bool mHeartbeat;
@@ -65,7 +70,6 @@ private:
     PosPoint convertMissionItemToPosPoint(const mavsdk::MissionRawServer::MissionItem &item);
     void handleManualControlMessage(mavlink_manual_control_t manualControl);
     double mManualControlMaxSpeed = 2.0; // [m/s]
-
 };
 
 #endif // MAVSDKVEHICLESERVER_H
