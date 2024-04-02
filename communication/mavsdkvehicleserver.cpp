f934f54303fa53d82bd4bbd3ba80dbffc0667f03
Peter Folkesson <peter.folkesson@ri.se>
2024-04-02 14:17:23 +0000

    Optional IP and port arguments added
    Enter optional IP and port arguments when starting RCCar

--- communication/mavsdkvehicleserver.cpp
+++ communication/mavsdkvehicleserver.cpp
@@ -13,7 +13,7 @@
 #include "WayWise/logger/logger.h"
 #include "WayWise/communication/parameterserver.h"
 
-MavsdkVehicleServer::MavsdkVehicleServer(QSharedPointer<VehicleState> vehicleState, const QHostAddress controlTowerAddress, const unsigned controlTowerPort, const QAbstractSocket::SocketType controlTowerSocketType) :
+MavsdkVehicleServer::MavsdkVehicleServer(QSharedPointer<VehicleState> vehicleState, QHostAddress controlTowerAddress, unsigned controlTowerPort, const QAbstractSocket::SocketType controlTowerSocketType) :
     VehicleServer(vehicleState)
 {
     connect(&Logger::getInstance(), &Logger::logSent, this, &MavsdkVehicleServer::on_logSent);
