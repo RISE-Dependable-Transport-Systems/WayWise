3f455024428d871c0a518fbd06892499bd6e3e18
rickaha <rickard.hall@ri.se>
2024-05-30 13:46:41 +0200

    Created namespace for geometric calculations

---
+++ core/geometry.h
@@ -0,0 +1,15 @@
+/*
+ *     Copyright 2024 Rickard Häll   rickard.hall@ri.se
+ *     Published under GPLv3: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Namespace to hold general geometric calculations
+ */
+
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+namespace geometry {
+
+}
+
+#endif // GEOMETRY_H
