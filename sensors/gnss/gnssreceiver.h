530d0dabf9b2b24e250a52a5b6411f1d2eaa5044
Ramana Avula <r2avula@gmail.com>
2025-03-27 22:10:01 +0100

    ubloxrover: add support for ublox-zed-f9r

--- sensors/gnss/gnssreceiver.h
+++ sensors/gnss/gnssreceiver.h
@@ -22,8 +22,9 @@ public:
 
     virtual llh_t getEnuRef() const { return mEnuReference; }
     virtual void setEnuRef(llh_t enuRef);
-    virtual void setIMUOrientationOffset(double roll_deg, double pitch_deg, double yaw_deg);
-    virtual void setGNSSPositionOffset(double xOffset, double yOffset);
+    virtual void setChipOrientationOffset(double roll_deg, double pitch_deg, double yaw_deg) { mAChipOrientationOffset = {roll_deg, pitch_deg, yaw_deg}; }
+    virtual void setAntennaToChipOffset(double xOffset, double yOffset, double zOffset) { mAntennaToChipOffset = {xOffset, yOffset, zOffset}; }
+    virtual void setChipToRearAxleOffset(double xOffset, double yOffset, double zOffset) { mChipToRearAxleOffset = {xOffset, yOffset, zOffset}; }
 
 signals:
     void updatedEnuReference(llh_t mEnuReference);
@@ -32,8 +33,9 @@ protected:
     llh_t mEnuReference;
     bool mEnuReferenceSet = false;
     QSharedPointer<VehicleState> mVehicleState;
-    struct {double rollOffset_deg, pitchOffset_deg, yawOffset_deg;} mIMUOrientationOffset;
-    xyz_t mGNSSPositionOffset = {0.0, 0.0, 0.0};
+    struct {double rollOffset_deg, pitchOffset_deg, yawOffset_deg;} mAChipOrientationOffset; // in degrees
+    xyz_t mAntennaToChipOffset = {0.0, 0.0, 0.0}; // in meters
+    xyz_t mChipToRearAxleOffset = {0.0, 0.0, 0.0}; // in meters
 };
 
 #endif // GNSSRECEIVER_H
--- sensors/gnss/ubloxrover.h
+++ sensors/gnss/ubloxrover.h
@@ -12,6 +12,8 @@
 #include <QSharedPointer>
 #include "ublox.h"
 #include "gnssreceiver.h"
+#include <QEventLoop>
+#include <QTimer>
 
 class UbloxRover : public GNSSReceiver
 {
@@ -21,8 +23,16 @@ public:
     bool connectSerial(const QSerialPortInfo &serialPortInfo);
     bool isSerialConnected();
     void writeRtcmToUblox(QByteArray data);
-    void writeOdoToUblox(ubx_esf_datatype_enum dataType, uint32_t dataField);
+    void writeOdomToUblox(ubx_esf_datatype_enum dataType, uint32_t dataField, uint32_t timeTag = 0);
     void saveOnShutdown();
+    void setUbloxVariant(UBLOX_VARIANT ubloxVariant) { mUbloxVariant = ubloxVariant; }
+    void setDynamicModel(DynamicModel dynamicModel) { mDynamicModel = dynamicModel; }
+    void setForceRecalibrateSensors(bool forceRecalibrateSensors) { mForceRecalibrateSensors = forceRecalibrateSensors; }
+    void setGNSSMeasurementRate(int rate) { mGNSSMeasurementRate = rate; }
+    void setNavPrioMessageRate(int rate) { mNavPrioMessageRate = rate; }
+    void setSpeedDataInputRate(int rate) { mSpeedDataInputRate = rate; }
+    void setPrintVerbose(bool printVerbose) { mPrintVerbose = printVerbose; }
+    void setESFAlgAutoMntAlgOn(bool esfAlgAutoMntAlgOn) { mESFAlgAutoMntAlgOn = esfAlgAutoMntAlgOn; }
 
 signals:
     void updatedGNSSPositionAndYaw(QSharedPointer<VehicleState> vehicleState, double distanceMoved, bool fused);
@@ -31,11 +41,23 @@ signals:
 
 private:
     bool configureUblox();
-    void updSosResponse(const ubx_upd_sos &sos);
     void updateGNSSPositionAndYaw(const ubx_nav_pvt &pvt);
+    void restoreBackedupConfiguration(int pollIntervalms = 1000, int maxPolls = 10);
+    void createConfigurationBackup(int pollIntervalms = 1000, int maxPolls = 10);
 
     const int ms_per_day = 24 * 60 * 60 * 1000;
     Ublox mUblox;
+    UBLOX_VARIANT mUbloxVariant = UBLOX_VARIANT::ZED_F9R;
+    UBLOX_STATE mUbloxState = UBLOX_STATE::UNKNOWN;
+    GNSS_FIX_TYPE mFixType = GNSS_FIX_TYPE::NO_FIX;
+
+    DynamicModel mDynamicModel = DynamicModel::AUTOMOT;
+    int mGNSSMeasurementRate = 5; // Hz
+    int mNavPrioMessageRate = 10; // Hz
+    int mSpeedDataInputRate = 10; // Hz
+    bool mForceRecalibrateSensors = false;
+    bool mESFAlgAutoMntAlgOn = false;
+    bool mPrintVerbose = false;
 };
 
 #endif // UBLOXROVER_H
